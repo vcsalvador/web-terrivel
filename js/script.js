7a241e58a0822dbd92c3adeb16e0329b53606241
Victor <victorcsalvador@gmail.com>
2014-10-29 00:19:31 +0000

    Trabalho terminado

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#tipo_pedras_sortidas').click(function(){
+            $('#tipoPedras').toggle()
+    })
+    $('#limpar').click(function() {
+        $('#tipoPedras').show()
+    })
+})
+
